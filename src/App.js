5b95ffa442af24b4eb8e01c7acff553f82f03283
Lee SeungHo <koreaflash@naver.com>
2021-06-02 16:31:44 +0900

    2021.06.02 from office

--- src/App.js
+++ src/App.js
@@ -3,42 +3,88 @@ import ViewerTemplate from "./components/ViewerTemplate/ViewerTemplate";
 import Viewer from "./components/Viewer";
 import * as api from './lib/api';
 import { Component } from "react";
+import moment from "moment";
 
 class App extends Component {
+  state = {
+    loading: false,
+    maxDate: null,
+    date: null,
+    url: null,
+    mediaType: null
+  }
+
   /*
   getAPOD = (date) => {
     api.getAPOD(date).then((res) => {
       console.log(res)
+    }).catch((e) => {
+      console.error(e);
     });
   }*/
 
+
   getAPOD = async (date) => {
+    if (this.state.loading) return;
+
+    this.setState({
+      loading: true
+    });
+
     try {
       const res = await api.getAPOD(date);
+      const { date: retrievedDate, url, media_type: mediaType } = res.data;
+      console.log(retrievedDate, url, mediaType);
+      if (!this.state.maxDate) {
+        this.setState({
+          maxDate: retrievedDate
+        });
+      }
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      });
+
       console.log(res);
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
+
+    this.setState({
+      loading: false
+    });
   }
 
   componentDidMount() {
     this.getAPOD();
   }
 
+  handleNext = () => {
+    const { date, maxDate } = this.state;
+    if (date === maxDate) return;
+    const nextDate = moment(date).add(1, 'days').format('YYYY-MM-DD');
+    this.getAPOD(nextDate);
+  }
+
+  handlePrev = () => {
+    const { date } = this.state;
+    const prevDate = moment(date).subtract(1, 'days').format('YYYY-MM-DD');
+    this.getAPOD(prevDate);
+  }
+
   render() {
+    const { url, mediaType, loading } = this.state;
+
     return (
       <div>
         <ViewerTemplate
-          spaceNavigator={<SpaceNavigator />}
+          spaceNavigator={<SpaceNavigator onNext={this.handleNext} onPrev={this.handlePrev} />}
           viewer={
-            /*
             <Viewer
-              url="https://apod.nasa.gov/apod/image/1712/GeminidsYinHao1024.jpg"
-              mediaType="image" />
-              */
-            <Viewer
-              url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
-              mediaType="video" />
+              url={url}
+              mediaType={mediaType}
+              loading={loading} />
           }
         />
       </div >
--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,9 +1,18 @@
 import classNames from 'classnames/bind';
 import styles from './Viewer.scss';
+import { ChasingDots } from 'better-react-spinkit';
 
 const cx = classNames.bind(styles);
 
 const Viewer = ({ mediaType, url, loading }) => {
+    if (loading) {
+        return (
+            <div className={cx('viewer')}>
+                <ChasingDots color="white" size={60} />
+            </div>
+        );
+    }
+
     return (
         <div className={cx('viewer')}>
             {
